01a3cb2d18396558f629ea3dba6dfe4badd73a8e
Venkat Naveen <venkat.naveen@petco.com>
2020-11-27 16:00:38 -0800

    adding tests and up updated readme

--- metadata.rb
+++ metadata.rb
@@ -5,7 +5,7 @@ license 'All Rights Reserved'
 description 'Installs/Configures github_action_runners'
 long_description 'Installs/Configures github_action_runners'
 version '0.1.13'
-chef_version '>= 12.14' if respond_to?(:chef_version)
+chef_version '>= 15.0' if respond_to?(:chef_version)
 
 # The `issues_url` points to the location where issues for this cookbook are
 # tracked.  A `View Issues` link will be displayed on this cookbook's page when
